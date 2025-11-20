482d670d94bcb5322ab512ac523a6dbd6c91baf6
k23163890 <k23163890@kcl.ac.uk>
2025-11-20 17:53:06 +0000

    Improved Deletion and Database Management

--- QuickSilver_6_2025/recipify/urls.py
+++ QuickSilver_6_2025/recipify/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('password/', views.PasswordView.as_view(), name='password'),
     path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
+    path('account/delete/', views.DeleteAccountView.as_view(), name='delete_account'),
     path('accounts/', include('allauth.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
